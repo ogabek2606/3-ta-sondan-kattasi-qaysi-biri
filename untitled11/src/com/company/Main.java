992f8390740f6a8b5f9806a160f48ba6fec2135c
ogabek2606 <110012533+ogabek2606@users.noreply.github.com>
2022-07-30 02:31:02 -0400

    Add files via upload

---
+++ untitled11/src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Scanner scanner =  new Scanner(in);
+
+
+
+
+                int a = 4;
+                int b = 71;
+                int c = 10;
+
+                if (a > b) {
+                    if (a > c) {
+                        System.out.println("katta son a = " + a);
+                    } else {
+                        System.out.println("katta son c = " + c);
+                    }
+                } else {
+                    if (b > c) {
+                        System.out.println("katta son b = " + b);
+                    } else {
+                        System.out.println("katta son c = " + c);
+                    }
+                }
+
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
